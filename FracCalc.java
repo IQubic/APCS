7b43371536b80cab116df9f70077a2fb844fed10
Avi Caspe <avi.caspe@gmail.com>
2016-11-10 15:08:43 -0800

    Done with checkpoint 1 for FracCalc

--- FracCalc.java
+++ FracCalc.java
@@ -20,12 +20,30 @@ public class FracCalc {
     
     //Runs all tests and prints the results of the tests
     public static void runTests() {
+        //Each pair of elements in this array goes together:
+        //tests[0] = The first test's input and test[1] = The first test's expected output
+        //tests[2] = The second test's input and test[3] = The second test's expected output
+        String[] tests = {"1/2 + 1/2", "1/2",
+                            "1_2/3 * 1_3/4" , "1_3/4",
+                            "2 / 3", "3",
+                            "1 - 1_2/3", "1_2/3"};
 
+        //Let's run the tests
+        int numOfTests = tests.length / 2;
+        int numOfTestsPassed = 0;
+        for (int i = 0; i < tests.length; i += 2) {
+            String testCase = tests[i];
+            String expectedResult = test[i + 1];
+            String actualResult = produceAnswer(testCase);
+            if (if actualResult.equals(expectedResult)) {
+                //Test passed
+                numOfTestsPassed++;
+            } else {
+                //Test failed
+                System.out.println(testCase + " returned: " + actualResult + ". Expected result was: " + expectedResult);
+            }
+        }
+        //After all tests are run
+        System.out.println(numOfTestsPassed + " out of " + numberOfTests + " passed");
     }
-    
-    //Runs a single test and sees if it passed
-    public static boolean testPassed(String input, String expectedOutput) {
-
-    }
-
 }
