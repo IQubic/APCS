34fa711f686e3fbb12536e84530e423dae48967e
Avi Caspe <avi.caspe@gmail.com>
2016-11-14 11:01:14 -0800

    Updated Test Method to use a 2D array

--- FracCalc.java
+++ FracCalc.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class FracCalc {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        
+
         //Print out a greeting
         System.out.println("Welcome to FracCalc!");
         System.out.println("Enter an equation or \"quit\" to quit");
@@ -21,25 +21,23 @@ public class FracCalc {
         String[] expression = input.split(" ");
         return expression[2];
     }
-    
+
     //Runs all tests and prints the results of the tests
     public static void runTests() {
-        //Each pair of elements in this array goes together:
-        //tests[0] = The first test's input and test[1] = The first test's expected output
-        //tests[2] = The second test's input and test[3] = The second test's expected output
-        String[] tests = {"1/2 + 1/2", "1/2",
-                            "1_2/3 * 1_3/4" , "1_3/4",
-                            "2 / 3", "3",
-                            "1 - 1_2/3", "1_2/3",
-                            "-3 * 4/2", "4/2",
-                            "1_1/2 - -4", "-4"};
+        //Each pair of elements in this array goes together
+        String[][] tests = {{"1/2 + 1/2", "1/2"},
+                            {"1_2/3 * 1_3/4" , "1_3/4"},
+                            {"2 / 3", "3"},
+                            {"1 - 1_2/3", "1_2/3"},
+                            {"-3 * 4/2", "4/2"},
+                            {"1_1/2 - -4", "-4"}};
 
         //Let's run the tests
-        int numOfTests = tests.length / 2;
+        int numOfTests = tests.length;
         int numOfTestsPassed = 0;
-        for (int i = 0; i < tests.length; i += 2) {
-            String testCase = tests[i];
-            String expectedResult = tests[i + 1];
+        for (String[] currentTest : tests) {
+            String testCase = currentTest[0];
+            String expectedResult = currentTest[1];
             String actualResult = produceAnswer(testCase);
             if ( actualResult.equals(expectedResult)) {
                 //Test passed
@@ -50,6 +48,6 @@ public class FracCalc {
             }
         }
         //After all tests are run
-        System.out.println(numOfTestsPassed + " out of " + numOfTests + " passed");
+        System.out.println(numOfTestsPassed + " out of " + numOfTests + " tests passed");
     }
 }
