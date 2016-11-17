44599ebff852b439b3da46df9924f5923a16d01e
Avi Caspe <avi.caspe@gmail.com>
2016-11-17 11:52:37 -0800

    Upadated the test methods to reflect the output of Chcekpoint 2 for FracCalc

--- FracCalc.java
+++ FracCalc.java
@@ -24,13 +24,15 @@ public class FracCalc {
 
     //Runs all tests and prints the results of the tests
     public static void runTests() {
-        //Each pair of elements in this array goes together
-        String[][] tests = {{"1/2 + 1/2", "1/2"},
-                            {"1_2/3 * 1_3/4" , "1_3/4"},
-                            {"2 / 3", "3"},
-                            {"1 - 1_2/3", "1_2/3"},
-                            {"-3 * 4/2", "4/2"},
-                            {"1_1/2 - -4", "-4"}};
+        //Each pair of elements in this array goes together:
+        //tests[i] = [test case, expected output]
+        String[][] tests = {{"1/2 + 1/2", "Whole: 0 Numerator: 1 Denominator: 2"},
+                            {"1_2/3 * 1_3/4" , "Whole: 1 Numerator: 3 Denominator: 4"},
+                            {"2 / 3", "Whole: 3 Numerator: 0 Denominator: 0"},
+                            {"1 - 1_2/3", "Whole: 1 Numerator: 2 Denominator: 3"},
+                            {"-3 * 4/2", "Whole: 0 Numerator: 4 Denominator: 2"},
+                            {"1_1/2 - -4", "Whole: -4 Numerator: 0 Denominator: 0"}
+                            {"1 + 1/-2", "Whole: 0 Numerator: 1 Denominator: -2"}};
 
         //Let's run the tests
         int numOfTests = tests.length;
