26b68716223d0db9e460105fc643be72f0753e41
Avi Caspe <avi.caspe@gmail.com>
2016-11-18 22:39:16 -0800

    Done with Checkpoint2 for FracCalc

--- FracCalc.java
+++ FracCalc.java
@@ -4,14 +4,14 @@ public class FracCalc {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
 
-        //Print out a greeting
+        // Print out a greeting
         System.out.println("Welcome to FracCalc!");
         System.out.println("Enter an equation or \"quit\" to quit");
 
-        //Parse input
+        // Parse input
         String input = console.nextLine().toLowerCase();
 
-        while (!input.equals("quit") {
+        while (!input.equals("quit")) {
             if (input.equals("test")) {
                 runTests();
             } else {
@@ -20,39 +20,57 @@ public class FracCalc {
             input = console.nextLine().toLowerCase();
         }
 
-        //Print a exit message
+        // Print a exit message
         System.out.println("Goodbye!");
     }
 
-    //A call to produceAnswer gives the result of an operation
+    // A call to produceAnswer gives the result of a given input
     public static String produceAnswer(String input) {
         String[] expression = input.split(" ");
         int[] frac1 = parseFraction(expression[0]);
         int[] frac2 = parseFraction(expression[2]);
-        return "Whole: " + frac2[0] + ", Numerator: " + frac2[1] + ", Denominator: " + frac2;
+        return "Whole: " + frac2[0] + ", Numerator: " + frac2[1] + ", Denominator: " + frac2[2];
     }
 
-    //parseFraction takes a String and returns an int array repersenting the fraction
-    //fraction[0] = Whole Number
-    //fraction[1] = Numerator
-    //fraction[2] = Denominator
+    // parseFraction takes a String and returns an int array repersenting the fraction
+    // fraction[0] = Whole Number
+    // fraction[1] = Numerator
+    // fraction[2] = Denominator
     public static int[] parseFraction(String fraction) {
-        //TODO Implement this method
+        // Split the fraction it's parts
+        String[] stringFraction = fraction.split("[_/]");
+
+        // Parse the Strings into ints
+        int[] intFraction = new int[stringFraction.length];
+        for (int i = 0; i < stringFraction.length; i++) {
+            intFraction[i] = Integer.parseInt(stringFraction[i]);
+        }
+
+        // Whole Number
+        if (intFraction.length == 1) {
+            return new int[] {intFraction[0], 0, 0};
+        // Just a Fraction
+        } else if (intFraction.length == 2) {
+            return new int[] {0, intFraction[0], intFraction[1]};
+        // Mixed Number
+        } else {
+            return new int[] {intFraction[0], intFraction[1], intFraction[2]};
+        }
     }
 
-    //Runs all tests and prints the results of the tests
+    // Runs all tests and prints the results of the tests
     public static void runTests() {
-        //Each pair of elements in this array goes together:
-        //tests[i] = [test case, expected output]
+        // Each pair of elements in this array goes together:
+        // tests[i] = [test case, expected output]
         String[][] tests = {{"1/2 + 1/2", "Whole: 0, Numerator: 1, Denominator: 2"},
                             {"1_2/3 * 1_3/4" , "Whole: 1, Numerator: 3, Denominator: 4"},
                             {"2 / 3", "Whole: 3, Numerator: 0, Denominator: 0"},
                             {"1 - 1_2/3", "Whole: 1, Numerator: 2, Denominator: 3"},
                             {"-3 * 4/2", "Whole: 0, Numerator: 4, Denominator: 2"},
-                            {"1_1/2 - -4", "Whole: -4, Numerator: 0, Denominator: 0"}
+                            {"1_1/2 - -4", "Whole: -4, Numerator: 0, Denominator: 0"},
                             {"1 + 1/-2", "Whole: 0, Numerator: 1, Denominator: -2"}};
 
-        //Let's run the tests
+        // Let's run the tests
         int numOfTests = tests.length;
         int numOfTestsPassed = 0;
         for (String[] currentTest : tests) {
@@ -60,14 +78,14 @@ public class FracCalc {
             String expectedResult = currentTest[1];
             String actualResult = produceAnswer(testCase);
             if ( actualResult.equals(expectedResult)) {
-                //Test passed
+                // Test passed
                 numOfTestsPassed++;
             } else {
-                //Test failed
-                System.out.println(testCase + " returned: " + actualResult + ". Expected result was: " + expectedResult);
+                // Test failed
+                System.out.println("\"" + testCase + "\" returned: " + actualResult + ". Expected result was: \"" + expectedResult + "\"");
             }
         }
-        //After all tests are run
+        // After all tests are run
         System.out.println(numOfTestsPassed + " out of " + numOfTests + " tests passed");
     }
 }
