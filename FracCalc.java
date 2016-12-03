ee778eb2e938a122e3067155965863ab56282751
Avi Caspe <avi.caspe@gmail.com>
2016-12-03 13:23:56 -0800

    Fixed divide by zero error

--- FracCalc.java
+++ FracCalc.java
@@ -84,6 +84,11 @@ public class FracCalc {
                     throw new IllegalArgumentException("Parse error on input: Illegal Operator \"" + nextToken + "\"");
                 }
             }
+
+            // Check for a divide by zero with a sace between the zero and the division symbol
+            if (fracs.get(fracs.size() - 1)[0] == 0 && operators.get(operators.size() - 1).equals("/")) {
+                throw new IllegalArgumentException("Divide by Zero Error");
+            }
         }
     }
 
@@ -108,6 +113,7 @@ public class FracCalc {
                 case "/": result = divFrac(fracs.get(0), fracs.get(1));
                           break;
             }
+
             fracs.remove(0);
             fracs.set(0, result);
             operators.remove(0);
