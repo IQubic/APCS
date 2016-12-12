d8be0a6a35bf6bfc6344964e5cae03cbc8539711
Avi Caspe <avi.caspe@gmail.com>
2016-12-12 01:02:32 -0800

    Fixed Line.java

--- Othello/Line.java
+++ Othello/Line.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 
 // This class represents a line of tiles
+// A line excludes the start tile as
+// That is where the last piece has been played
+// And that piece doesn't need to be flipped
 public class Line {
     private ArrayList<Tile> tiles;
     private Direction dir;
 
     public Line(Tile start, Direction dir) {
         this.tiles = new ArrayList<>();
-        this.tiles.add(start);
+        this.tiles.add(start.nextTile(dir));
         this.dir = dir;
     }
 
